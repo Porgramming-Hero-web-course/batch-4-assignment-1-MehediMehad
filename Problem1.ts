b240834bd7dce653857035bc33d99e67040302ed
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2024-11-06 21:13:56 +0600

    Problem 1 solve

--- Problem1.ts
+++ Problem1.ts
@@ -1,6 +1,13 @@
-type NumberArray = number []
 
-const array : NumberArray = [1, 5, 6, 8]
-console.log(array);
+// Type alias 'NumberArray' to define an array of numbers
+type NumberArray = number[];
 
+// Function 'sumArray' to calculate the sum of elements in an array
+const sumArray = (array: NumberArray): number => {
+  const sum = array.reduce((totalSum: number, currentValue: number) => totalSum + currentValue, 0);
+  console.log(sum);
+  return sum;
+};
+
+sumArray([1, 2, 3, 4, 5, 5, 5]);
 
