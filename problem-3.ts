572af7fd0ed2dea03c5552f1eb205949766ccea8
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2024-11-07 11:36:50 +0600

    Problem 3 solve

--- problem-3.ts
+++ problem-3.ts
@@ -2,22 +2,22 @@
   function countWordOccurrences(sentence: string, word: string): number {
 
     // Convert sentence and word to lowercase 
-    const sentenceLower: string = sentence.toLowerCase();
-    const wordLower: string = word.toLowerCase();
+    const sentenceLower: string = sentence.toLowerCase()
+    const wordLower: string = word.toLowerCase()
 
     // Split the sentence into words []
     const partWord: string[] = sentenceLower.split(" ");
     
     // Filter the words in the sentence and return the number of exact matches with the target word
-    const count: number = partWord.filter((w) => w === wordLower).length;
-    console.log(count);
+    const count: number = partWord.filter((w) => w === wordLower).length
+    console.log(count)
 
-    return count;
+    return count
   }
 
 
 
-  countWordOccurrences("Hello world! This is Hello Hello Hello I love Hello world a Hello again world. Hello", "hello");
+  countWordOccurrences("TypeScript is great. I love TypeScript", "typescript");
 
 
 
